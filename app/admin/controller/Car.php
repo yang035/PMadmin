e2803e1c73649b710017845bb820ec7a5b9149c0
ernest <ernest96@yeah.net>
2019-01-03 11:49:42 +0800

    添加车辆信息

---
+++ app/admin/controller/Car.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/8
+ * Time: 14:59
+ */
+
+namespace app\admin\controller;
+use app\admin\model\CarCat as CatModel;
+use app\admin\model\CarItem as ItemModel;
+
+
+class Car extends Admin
+{
+    public $tab_data = [];
+    protected function _initialize()
+    {
+        parent::_initialize();
+
+        $tab_data['menu'] = [
+            [
+                'title' => '车辆类型',
+                'url' => 'admin/Car/cat',
+            ],
+            [
+                'title' => '车辆信息',
+                'url' => 'admin/Car/index',
+            ],
+        ];
+        $this->tab_data = $tab_data;
+    }
+
+    public function index($q = '')
+    {
+        if ($this->request->isAjax()) {
+            $where = $data = [];
+            $page = input('param.page/d', 1);
+            $limit = input('param.limit/d', 20);
+
+            $cat_id = input('param.cat_id/d');
+            if ($cat_id){
+                $where['cat_id'] = $cat_id;
+            }
+            $name = input('param.name');
+            if ($name) {
+                $where['name'] = ['like', "%{$name}%"];
+            }
+            $where['cid'] = session('admin_user.cid');
+            $data['data'] = ItemModel::with('cat')->where($where)->page($page)->limit($limit)->select();
+            $carType = config('other.car_color');
+            if ($data['data']){
+                foreach ($data['data'] as $k=>$v){
+                    $v['color'] = $carType[$v['color']];
+                }
+            }
+            $data['count'] = ItemModel::where($where)->count('id');
+            $data['code'] = 0;
+            $data['msg'] = '';
+            return json($data);
+        }
+
+        // 分页
+        $tab_data = $this->tab_data;
+        $tab_data['current'] = url('');
+
+        $this->assign('tab_data', $tab_data);
+        $this->assign('tab_type', 1);
+        $this->assign('cat_option',ItemModel::getOption());
+        return $this->fetch('item');
+    }
+    public function addItem()
+    {
+        if ($this->request->isPost()) {
+            $data = $this->request->post();
+            // 验证
+            $result = $this->validate($data, 'CarItem');
+            if($result !== true) {
+                return $this->error($result);
+            }
+            $data['cid'] = session('admin_user.cid');
+            $data['user_id'] = session('admin_user.uid');
+            unset($data['id']);
+            if (!ItemModel::create($data)) {
+                return $this->error('添加失败');
+            }
+            return $this->success("操作成功{$this->score_value}");
+        }
+        $this->assign('cat_option',ItemModel::getOption());
+        $this->assign('car_color',ItemModel::getColorOption());
+        return $this->fetch('itemform');
+    }
+
+    public function editItem($id = 0)
+    {
+        if ($this->request->isPost()) {
+            $data = $this->request->post();
+            // 验证
+            $result = $this->validate($data, 'CarItem');
+            if($result !== true) {
+                return $this->error($result);
+            }
+            $data['cid'] = session('admin_user.cid');
+            $data['user_id'] = session('admin_user.uid');
+            if (!ItemModel::update($data)) {
+                return $this->error('修改失败');
+            }
+            return $this->success('修改成功');
+        }
+
+        $row = ItemModel::where('id', $id)->find()->toArray();
+        $this->assign('data_info', $row);
+        $this->assign('cat_option',ItemModel::getOption());
+        $this->assign('car_color',ItemModel::getColorOption());
+        return $this->fetch('itemform');
+    }
+
+    public function delItem()
+    {
+        $id = input('param.id/a');
+        $model = new ItemModel();
+        if (!$model->del($id)) {
+            return $this->error($model->getError());
+        }
+        return $this->success('删除成功');
+    }
+
+    public function cat()
+    {
+        if ($this->request->isAjax()) {
+            $where = $data = [];
+            $page = input('param.page/d', 1);
+            $limit = input('param.limit/d', 15);
+            $keyword = input('param.keyword');
+            if ($keyword) {
+                $where['name'] = ['like', "%{$keyword}%"];
+            }
+
+            $data['data'] = CatModel::where($where)->page($page)->limit($limit)->select();
+            $data['count'] = CatModel::where($where)->count('id');
+            $data['code'] = 0;
+            $data['msg'] = '';
+            return json($data);
+        }
+
+        $tab_data = $this->tab_data;
+        $tab_data['current'] = url('');
+        $this->assign('tab_data', $tab_data);
+        $this->assign('tab_type', 1);
+        return $this->fetch();
+    }
+
+    public function addCat()
+    {
+        if ($this->request->isPost()) {
+            $data = $this->request->post();
+            // 验证
+            $result = $this->validate($data, 'CarCat');
+            if($result !== true) {
+                return $this->error($result);
+            }
+            $data['cid'] = session('admin_user.cid');
+            $data['user_id'] = session('admin_user.uid');
+            unset($data['id']);
+            if (!CatModel::create($data)) {
+                return $this->error('添加失败');
+            }
+            return $this->success("操作成功{$this->score_value}");
+        }
+        return $this->fetch('catform');
+    }
+
+    public function editCat($id = 0)
+    {
+        if ($this->request->isPost()) {
+            $data = $this->request->post();
+            // 验证
+            $result = $this->validate($data, 'CarCat');
+            if($result !== true) {
+                return $this->error($result);
+            }
+            $data['cid'] = session('admin_user.cid');
+            $data['user_id'] = session('admin_user.uid');
+            if (!CatModel::update($data)) {
+                return $this->error('修改失败');
+            }
+            return $this->success('修改成功',url('cat'));
+        }
+
+        $row = CatModel::where('id', $id)->find()->toArray();
+        $this->assign('data_info', $row);
+        return $this->fetch('catform');
+    }
+    public function delCat()
+    {
+        $id = input('param.id/a');
+        $model = new CatModel();
+        if (!$model->del($id)) {
+            return $this->error('此类别下有检查项，不能删除');
+        }
+        return $this->success('删除成功');
+    }
+
+}
---
+++ app/admin/validate/CarItem.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/8
+ * Time: 15:02
+ */
+
+namespace app\admin\validate;
+
+
+use think\Validate;
+
+class CarItem extends Validate
+{
+    //定义验证规则
+    protected $rule = [
+        'name|名称' => 'require|unique:contract_item',
+        'color|颜色' => 'require',
+        'idcard|车牌号' => 'require',
+        'status|状态设置'  => 'require|in:0,1',
+    ];
+
+    //定义验证提示
+    protected $message = [
+        'name.require' => '请输入名称',
+        'name.unique' => '名称已存在',
+        'color.require' => '车辆颜色必填',
+        'idcard.require' => '车牌号必填',
+        'status.require'    => '请设置状态',
+    ];
+
+}
