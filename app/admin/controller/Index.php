a15c734ae5cfdc58e0299bb4dd8b03c149551b76
ernest <ernest96@yeah.net>
2019-01-16 09:38:14 +0800

    添加汇总统计

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -2,6 +2,9 @@
 namespace app\admin\controller;
 
 use app\common\util\Dir;
+use app\admin\model\Project as ProjectModel;
+use app\admin\model\Approval as ApprovalModel;
+use app\admin\model\DailyReport as DailyReportModel;
 
 class Index extends Admin
 {
@@ -11,6 +14,78 @@ class Index extends Admin
             $this->view->engine->layout(false);
             return $this->fetch('iframe');
         } else {
+            $map = [
+                'cid'=>session('admin_user.cid'),
+                't_type'=>1,
+            ];
+            $uid = session('admin_user.uid');
+            $project_fields = "SUM(IF(JSON_CONTAINS_PATH(deal_user,'one', '$.\"$uid\"') AND send_user LIKE '%a%',1,0)) deal_num,
+        SUM(IF(JSON_CONTAINS_PATH(manager_user,'one', '$.\"$uid\"'),1,0)) manager_num,
+        SUM(IF(JSON_EXTRACT(send_user,'$.\"$uid\"') = '',1,0)) send_num,
+        SUM(IF(JSON_CONTAINS_PATH(copy_user,'one', '$.\"$uid\"'),1,0)) copy_num,
+        SUM(IF(JSON_EXTRACT(send_user,'$.\"$uid\"') = 'a',1,0)) has_num";
+            $sta_count['project'] = ProjectModel::field($project_fields)->where($map)->find()->toArray();
+
+            $map = [
+                'cid'=>session('admin_user.cid'),
+                't_type'=>2,
+            ];
+            $task_fields = "SUM(IF(JSON_CONTAINS_PATH(deal_user,'one', '$.\"$uid\"') AND send_user LIKE '%a%',1,0)) deal_num,
+        SUM(IF(JSON_CONTAINS_PATH(manager_user,'one', '$.\"$uid\"'),1,0)) manager_num,
+        SUM(IF(JSON_EXTRACT(send_user,'$.\"$uid\"') = '',1,0)) send_num,
+        SUM(IF(JSON_CONTAINS_PATH(copy_user,'one', '$.\"$uid\"'),1,0)) copy_num,
+        SUM(IF(JSON_EXTRACT(send_user,'$.\"$uid\"') = 'a',1,0)) has_num";
+            $sta_count['task'] = ProjectModel::field($task_fields)->where($map)->find()->toArray();
+
+            $map = [
+                'cid'=>session('admin_user.cid'),
+            ];
+            $approval_fields = "SUM(IF(user_id='{$uid}',1,0)) user_num,
+        SUM(IF(JSON_CONTAINS_PATH(send_user,'one', '$.\"$uid\"') and status=1,1,0)) send_num,
+        SUM(IF(JSON_CONTAINS_PATH(copy_user,'one', '$.\"$uid\"'),1,0)) copy_num,
+        SUM(IF(JSON_CONTAINS_PATH(deal_user,'one', '$.\"$uid\"'),1,0)) deal_num,
+        SUM(IF(JSON_CONTAINS_PATH(send_user,'one', '$.\"$uid\"') and status>1,1,0)) has_num";
+            $sta_count['approval'] = ApprovalModel::field($approval_fields)->where($map)->find()->toArray();
+
+            $map = [
+                'cid'=>session('admin_user.cid'),
+            ];
+            $daily_fields = "SUM(IF(user_id='{$uid}',1,0)) user_num,
+        SUM(IF(JSON_EXTRACT(send_user,'$.\"$uid\"') = '',1,0)) send_num,
+        SUM(IF(JSON_CONTAINS_PATH(copy_user,'one', '$.\"$uid\"'),1,0)) copy_num,
+        SUM(IF(JSON_EXTRACT(send_user,'$.\"$uid\"') = 'a',1,0)) has_num";
+            $sta_count['daily'] = DailyReportModel::field($daily_fields)->where($map)->find()->toArray();
+
+            $name = [
+                'send_num'=>'待我审批',
+                'copy_num'=>'抄送我的',
+                'has_num'=>'已审批的',
+                'manager_num'=>'我负责的',
+                'deal_num'=>'我参与的',
+                'user_num'=>'我的申请/汇报',
+            ];
+            $data = [];
+            if ($sta_count) {
+                $tmp = array_values($sta_count);
+                foreach ($tmp as $k => $v) {
+                    foreach ($name as $key => $value) {
+                        if (!array_key_exists($key, $v)) {
+                            $tmp[$k][$key] = 0;
+                        }
+                    };
+                    foreach ($name as $key => $value) {
+                        $data[$key]['type'] = 'bar';
+                        $data[$key]['name'] = $value;
+                        $data[$key]['data'] = array_column($tmp, $key);
+                    }
+
+                }
+            }
+
+            $title = ['ML工作','ML临时','日常审批','日报'];
+            $this->assign('x', json_encode(array_values($name)));
+            $this->assign('y', json_encode($title));
+            $this->assign('data', json_encode(array_values($data)));
             return $this->fetch();
         }
     }
--- app/admin/view/index/index.php
+++ app/admin/view/index/index.php
@@ -1,2 +1,47 @@
-此功能待开放
+<div id="container" style="height: 500px"></div>
+<script type="text/javascript" src="http://echarts.baidu.com/gallery/vendors/echarts/echarts.min.js"></script>
+<script type="text/javascript" src="http://echarts.baidu.com/gallery/vendors/echarts-gl/echarts-gl.min.js"></script>
+<script type="text/javascript" src="http://echarts.baidu.com/gallery/vendors/echarts-stat/ecStat.min.js"></script>
+<script type="text/javascript" src="http://echarts.baidu.com/gallery/vendors/echarts/extension/dataTool.min.js"></script>
+<script type="text/javascript" src="http://echarts.baidu.com/gallery/vendors/echarts/map/js/china.js"></script>
+<script type="text/javascript" src="http://echarts.baidu.com/gallery/vendors/echarts/map/js/world.js"></script>
+<script type="text/javascript" src="https://api.map.baidu.com/api?v=2.0&ak=ZUONbpqGBsYGXNIYHicvbAbM"></script>
+<script type="text/javascript" src="http://echarts.baidu.com/gallery/vendors/echarts/extension/bmap.min.js"></script>
+<script type="text/javascript" src="http://echarts.baidu.com/gallery/vendors/simplex.js"></script>
+<script type="text/javascript">
+    var dom = document.getElementById("container");
+    var myChart = echarts.init(dom);
+    var app = {};
+    option = null;
+    app.title = '数据汇总';
+
+    option = {
+        tooltip : {
+            trigger: 'axis',
+            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        legend: {
+            data: {$x}
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis:  {
+            type: 'value'
+        },
+        yAxis: {
+            type: 'category',
+            data: {$y}
+        },
+        series: {$data}
+    };
+    if (option && typeof option === "object") {
+        myChart.setOption(option, true);
+    }
+</script>
 {include file="block/layui" /}
