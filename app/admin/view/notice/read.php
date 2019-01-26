0838f3fda0fada24c21f8aeaf71df6943e64207b
ernest <ernest96@yeah.net>
2019-01-26 10:22:52 +0800

    修改bug

--- app/admin/view/notice/read.php
+++ app/admin/view/notice/read.php
@@ -8,3 +8,20 @@
         {$data_info['content']}
     </div>
 </div>
+{include file="block/layui" /}
+<script>
+    var formData = {:json_encode($data_info)};
+
+    layui.use(['jquery', 'laydate'], function() {
+        var $ = layui.jquery, laydate = layui.laydate;
+        laydate.render({
+            elem: '.field-expire_time',
+            min:'0'
+        });
+
+        $('#reset_expire').on('click', function(){
+            $('input[name="expire_time"]').val(0);
+        });
+    });
+</script>
+<script src="__ADMIN_JS__/footer.js"></script>
