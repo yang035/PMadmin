f5c55cd9fa4a24b2826e5440773fcc118901a394
ernest <ernest96@yeah.net>
2019-01-28 20:13:22 +0800

    修改bug

--- app/admin/validate/Project.php
+++ app/admin/validate/Project.php
@@ -7,19 +7,20 @@ class Project extends Validate
 {
     //定义验证规则
     protected $rule = [
-        'name|名称' => 'require|unique:project',
+        'name|名称' => 'require',
         'remark|描述' => 'require',
         'score|分值' => 'require',
         'start_time|开始时间' => 'require|date',
         'end_time|结束时间' => 'require|date',
         'send_user|审批人'   => 'require',
         'reason|事由'   =>'length:0,65',
+        'pid-name' => 'unique:project,pid^name',
     ];
 
     //定义验证提示
     protected $message = [
         'name.require' => '请填写项目名称',
-        'name.unique' => '名称已经存在',
+        'pid-name.unique' => '同项目下节点名称不能重复',
         'remark.require' => '请填写项目描述',
         'score.require' => '请填写项目预设分值',
         'start_time.require' => '选择开始时间',
