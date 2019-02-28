d5266bf8ced9799d8fc45f3ad103baa40e219f30
ernest <ernest96@yeah.net>
2019-02-28 17:57:08 +0800

    添加日常工作计划，主要针对行政类岗位

--- app/admin/validate/ApprovalProcurement.php
+++ app/admin/validate/ApprovalProcurement.php
@@ -10,7 +10,7 @@ class ApprovalProcurement extends Validate
         'name|名称' => 'require',
         'number|数量' => 'require',
         'amount|总价' => 'require',
-        'reason|事由'   =>'length:0,65',
+        'reason|事由'   =>'length:0,255',
     ];
 
     //定义验证提示
@@ -18,6 +18,6 @@ class ApprovalProcurement extends Validate
         'name.require' => '名称必填',
         'number.require' => '数量必填',
         'amount.require' => '总价必填',
-        'reason.length' => '事由超过限制65个字符数',
+        'reason.length' => '事由超过限制255个字符数',
     ];
 }
