282591ba1d4d41aece678f2a838c8683c239b1ba
ernest <ernest96@yeah.net>
2019-04-08 08:35:09 +0800

    调整已审批的数据显示

--- app/admin/view/approval/statistics.php
+++ app/admin/view/approval/statistics.php
@@ -26,42 +26,42 @@
                 <input type="button" class="layui-btn layui-btn-primary layui-icon export_btn" value="导出">
             </div>
         </form>
-        <div class="layui-form">
-            <table class="layui-table mt10" lay-even="" lay-skin="row">
-                <colgroup>
-                    <col width="50">
-                </colgroup>
-                <thead>
-                <tr>
-                    <th><input type="checkbox" lay-skin="primary" lay-filter="allChoose"></th>
-                    <th>姓名</th>
-                    {volist name="panel_type" id="vo"}
-                    <th>{$vo['title']}</th>
-                    {/volist}
-                </tr>
-                </thead>
-                <tbody>
-                {volist name="data_list" id="vo"}
-                <tr>
-                    <td><input type="checkbox" name="ids[]" class="layui-checkbox checkbox-ids" value="{$vo['id']}" lay-skin="primary"></td>
-                    <td class="font12">
-                        {$vo['realname']}
-                    </td>
-                    {volist name="panel_type" id="v"}
-                    <td class="font12">
-                        {empty name="vo['num_'.$key]"}0
-                        {else/}
-                        <strong style="color: red">{$vo['num_'.$key]}</strong>
-                        {/empty}
-                    </td>
-                    {/volist}
-                </tr>
-                {/volist}
-                </tbody>
-            </table>
-            {$pages}
-        </div>
     </div>
+</div>
+<div class="layui-form">
+    <table class="layui-table mt10" lay-even="" lay-skin="row">
+        <colgroup>
+            <col width="50">
+        </colgroup>
+        <thead>
+        <tr>
+            <th><input type="checkbox" lay-skin="primary" lay-filter="allChoose"></th>
+            <th>姓名</th>
+            {volist name="panel_type" id="vo"}
+            <th>{$vo['title']}</th>
+            {/volist}
+        </tr>
+        </thead>
+        <tbody>
+        {volist name="data_list" id="vo"}
+        <tr>
+            <td><input type="checkbox" name="ids[]" class="layui-checkbox checkbox-ids" value="{$vo['id']}" lay-skin="primary"></td>
+            <td class="font12">
+                {$vo['realname']}
+            </td>
+            {volist name="panel_type" id="v"}
+            <td class="font12">
+                {empty name="vo['num_'.$key]"}0
+                {else/}
+                <strong style="color: red">{$vo['num_'.$key]}</strong>
+                {/empty}
+            </td>
+            {/volist}
+        </tr>
+        {/volist}
+        </tbody>
+    </table>
+    {$pages}
 </div>
 
 {include file="block/layui" /}
