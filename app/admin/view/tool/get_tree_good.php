b6e349fe64a3663e3ef7d443b51bcc25e1e7f1a4
ernest <ernest96@yeah.net>
2019-04-19 15:15:51 +0800

    选择人员和物品实现可智能化搜索

--- app/admin/view/tool/get_tree_good.php
+++ app/admin/view/tool/get_tree_good.php
@@ -23,7 +23,7 @@
 <div>
     <div class="dtree" id="dtree_div" style="width: 500px;float: left">
         <p><button class="layui-btn layui-btn-normal" onclick="javascript:  d.openAll();">打开</button><button  class="layui-btn layui-btn-normal" onclick="javascript: d.closeAll();">关闭</button></p>
-        <input class="layui-input" id="dosearch_text" type="text" onkeyup="nodeSearching()"/><input id="dosearch" type="button" class="layui-btn layui-btn-normal" value="查询" onclick="nodeSearching() " />
+        <input class="layui-input" id="dosearch_text" type="text" onkeyup="nodeSearching()" placeholder="关键字搜索"/><input id="dosearch" type="button" class="layui-btn layui-btn-normal" value="查询" onclick="nodeSearching() " />
         <div style="color: red">备注：点击部门名称“批量操作”</div>
         <script type="text/javascript">
             d = new dTree('d', true);
--- app/admin/view/tool/get_tree_user.php
+++ app/admin/view/tool/get_tree_user.php
@@ -23,7 +23,7 @@
 <div>
     <div class="dtree" id="dtree_div" style="width: 500px;float: left">
         <p><button class="layui-btn layui-btn-normal" onclick="javascript:  d.openAll();">打开</button><button  class="layui-btn layui-btn-normal" onclick="javascript: d.closeAll();">关闭</button></p>
-        <input class="layui-input" id="dosearch_text" type="text" /><input id="dosearch" type="button" class="layui-btn layui-btn-normal" value="查询" onclick="nodeSearching() " />
+        <input class="layui-input" id="dosearch_text" type="text" onkeyup="nodeSearching()" placeholder="关键字搜索"/><input id="dosearch" type="button" class="layui-btn layui-btn-normal" value="查询" onclick="nodeSearching() " />
         <div style="color: red">备注：点击部门名称“批量操作”</div>
         <script type="text/javascript">
             d = new dTree('d', true);
@@ -137,10 +137,28 @@
             function nodeSearching() {
                 var dosearch = $.trim($("#dosearch_text").val());//获取要查询的文字
                 var dtree_div = $("#dtree_div").find(".dtree_node").show().filter(":contains('" + dosearch + "')");//获取所有包含文本的节点
-                $.each(dtree_div, function (index, element) {
-                    var s = $(element).attr("node_id");
-                    d.openTo(s);//根据id打开节点
-                });
+                if (dtree_div.length > 0){
+                    var r = $('#dd0');
+                    depNone(r.children()[1]);
+                    $.each(dtree_div, function (index, element) {
+                        var s = $(element).attr("node_id");
+                        $($(element).parent().parent()).css({display: 'block'});
+                        d.openTo(s);//根据id打开节点
+                    });
+                } else {
+                    layer.msg('关键字搜索不到');
+                }
+            }
+            function depNone(n) {
+                var c = $(n).children();
+                if (c.length > 0){
+                    $.each(c, function (index, element) {
+                        if ($(element).hasClass('dTreeNode') || $(element).hasClass('clip')){
+                            $(element).css({display: 'none'});
+                            depNone(element);
+                        }
+                    });
+                }
             }
 
             //判断默认选中复选框
