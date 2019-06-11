f8a22dada096d6584cff8f3e66f6aa399d56b7e4
ernest <ernest96@yeah.net>
2019-06-11 20:47:30 +0800

    审批可以显示excel，word等附件

--- app/admin/view/approval/read.php
+++ app/admin/view/approval/read.php
@@ -148,11 +148,14 @@
             事由：{$data_list['reason']}<br>
             附件说明：
             {notempty name="data_list['attachment'][0]"}
-            <div class="image-list">
+<!--            <div class="image-list">-->
+            <ul>
                 {volist name="data_list['attachment']" id="vo"}
-                <div class="cover"><img src="{$vo}" style="height: 30px;width: 30px;"></div>
+<!--                <div class="cover"><img src="{$vo}" style="height: 30px;width: 30px;"></div>-->
+                <li>&nbsp;&nbsp;&nbsp;&nbsp;<a target="_blank" href="{$vo}" style="color: #5c90d2">附件{$i}</a></li>
                 {/volist}
-            </div>
+            </ul>
+<!--            </div>-->
             {else/}
             <span>无</span>
             {/notempty}
