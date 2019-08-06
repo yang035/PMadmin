8ac4bc5f4661a33de37c524ec2a1f3d324669c9c
ernest <ernest96@yeah.net>
2019-08-06 17:26:14 +0800

    调整样式

--- app/admin/view/shop_order/index.php
+++ app/admin/view/shop_order/index.php
@@ -4,8 +4,8 @@
         width: auto;
     }
 </style>
-<div class="page-toolbar">
-    <div class="page-filter">
+<div>
+    <div>
         <form class="layui-form layui-form-pane" action="{:url()}" method="get" id="search_form">
             <div class="layui-form-item">
                 <div class="layui-inline">
