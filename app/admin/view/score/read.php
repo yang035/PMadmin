9cf238f9aaa9e3f2c40dc7facf6b9c998aaf460d
ernest <ernest96@yeah.net>
2020-05-22 15:49:22 +0800

    ML看板调整

--- app/admin/view/score/read.php
+++ app/admin/view/score/read.php
@@ -2,15 +2,18 @@
     <div class="layui-card">
         <div class="layui-card-body">
             {volist name="tmp" id="vo"}
-            <b>姓名：</b>{$user[$vo['uid']]}<br>
+<!--            <b>姓名：</b>{$user[$vo['uid']]}<br>-->
+            <b>员工编号：</b>{$vo['uid']}<br>
             <b>累计ML：</b>{$vo['ml']}<br>
             <b>已完成ML：</b>{$vo['finish_ml']}<br>
-            <b>已发放ML：</b>{$vo['total_fafang']}<br>
-            <b>未发放ML：</b>{$vo['finish_ml']-$vo['total_fafang']}<br>
+<!--            <b>已发放ML：</b>{$vo['total_fafang']}<br>-->
+<!--            <b>未发放ML：</b>{$vo['finish_ml']-$vo['total_fafang']}<br>-->
             <b>未完成ML：</b>{$vo['ml']-$vo['finish_ml']}<br>
             <b>当月完成：</b>{$vo['finish_ml_month']}<br>
             <b>当月发放：</b>{$vo['benci_fafang']}<br>
             <b>累计GL：</b>{$gl[$vo['uid']]['gl_add_sum']}<br>
+            <b>当月GL：</b>{$gl_month[$vo['uid']]['gl_add_sum']}<br>
+            <b>当月GL排名：</b>{$gl_month[$vo['uid']]['sort']}<br>
             {/volist}
         </div>
     </div>
