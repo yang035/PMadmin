e108369149b1b667dfadff8c1cfadf4e94ae4eb4
ernest <ernest96@yeah.net>
2020-05-29 09:31:29 +0800

    首页调整

--- app/index/view/index/index.php
+++ app/index/view/index/index.php
@@ -39,14 +39,14 @@
 <div class="gtco-section-overflow">
     <div class="row" style="margin-top: 300px;margin-bottom: 250px">
         <div class="col-md-12 col-xs-12 text-center gtco-heading">
-            <a href="http://imlgl.com" target="_blank"><img style="width: 80%;height: 80%" src="__PUBLIC_JS__/index/images/lo.png" title="进入工作"></a>
+            <a href="http://imlgl.com" target="_blank"><img style="width: 60%;height: 60%" src="__PUBLIC_JS__/index/images/lo.png" title="进入工作"></a>
         </div>
     </div>
-    <div class="jq22-container">
-        <div class="jq22-content bgcolor-3">
-            <div id="div1">
-                <div class="box col-md-2 col-xs-3"><img style="width: 100%" src="__PUBLIC_JS__/index/images/chan.png" alt=""></div>
-            </div>
-        </div>
-    </div>
+<!--    <div class="jq22-container">-->
+<!--        <div class="jq22-content bgcolor-3">-->
+<!--            <div id="div1">-->
+<!--                <div class="box col-md-2 col-xs-3"><img style="width: 100%" src="__PUBLIC_JS__/index/images/chan.png" alt=""></div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
 </div>
