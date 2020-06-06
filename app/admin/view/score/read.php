91b6b5ba8371f530864b5a506d7d2445bb5da70b
ernest <ernest96@yeah.net>
2020-06-06 18:23:44 +0800

    调整友好展示

--- app/admin/view/score/read.php
+++ app/admin/view/score/read.php
@@ -1,6 +1,7 @@
 <form class="layui-form layui-form-pane" action="{:url()}" method="post" id="editForm">
     <div class="layui-card">
         <div class="layui-card-body">
+            {notempty name="tmp"}
             {volist name="tmp" id="vo"}
             <b>员工编号：</b>{$user[$vo['uid']]['id_card']}<br>
             <b>累计ML：</b>{$vo['ml']}<br>
@@ -11,9 +12,12 @@
             <b>当月完成：</b>{$vo['finish_ml_month']}<br>
             <b>当月发放：</b>{$vo['benci_fafang']}<br>
             <b>累计GL：</b>{$gl[$vo['uid']]['gl_add_sum']}<br>
-            <b>当月GL：</b>{$gl_month[$vo['uid']]['gl_add_sum']}<br>
-            <b>当月GL排名：</b>{$gl_month[$vo['uid']]['sort']}<br>
+            <b>当月GL：</b>{$gl_month[$vo['uid']]['gl_add_sum']|default=0}<br>
+            <b>当月GL排名：</b>{$gl_month[$vo['uid']]['sort']|default=0}<br>
             {/volist}
+            {else/}
+            暂无发放ML
+            {/notempty}
         </div>
     </div>
 </form>
