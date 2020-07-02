99dc491007023fcacaaaf21ea2b29cd28a3676ac
ernest <ernest96@yeah.net>
2020-07-02 16:12:54 +0800

    获取全国公共资源交易平台数据

---
+++ app/index/model/GgzyDetail.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/7/1
+ * Time: 11:34
+ */
+namespace app\index\model;
+
+use think\Model;
+class GgzyDetail extends Model
+{
+    // 自动写入时间戳
+    protected $autoWriteTimestamp = true;
+}
---
+++ app/index/model/GgzyList.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/7/1
+ * Time: 11:34
+ */
+namespace app\index\model;
+
+use think\Model;
+class GgzyList extends Model
+{
+    // 自动写入时间戳
+    protected $autoWriteTimestamp = true;
+}
