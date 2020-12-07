6114be696d8d679d3a1d0bb0d0330ea352e70333
ernest <ernest96@yeah.net>
2020-12-07 11:46:58 +0800

    调整

--- app/admin/view/subject/item.php
+++ app/admin/view/subject/item.php
@@ -37,12 +37,14 @@
             </div>
         </form>
     </div>
+    {empty name="Request.param.param"}
     <div class="layui-btn-group fl">
         <a href="{:url('addItem')}" class="layui-btn layui-btn-primary layui-icon layui-icon-add-circle-fine">&nbsp;添加</a>
         <a data-href="{:url('status?table=subject_item&val=1')}" class="layui-btn layui-btn-primary j-page-btns layui-icon layui-icon-play" data-table="dataTable">&nbsp;启用</a>
         <a data-href="{:url('status?table=subject_item&val=0')}" class="layui-btn layui-btn-primary j-page-btns layui-icon layui-icon-pause" data-table="dataTable">&nbsp;禁用</a>
         <!--            <a data-href="{:url('delItem')}" class="layui-btn layui-btn-primary j-page-btns confirm layui-icon layui-icon-close red">&nbsp;删除</a>-->
     </div>
+    {/empty}
 </div>
 <table id="dataTable" class="layui-table" lay-filter="table1"></table>
 {include file="block/layui" /}
