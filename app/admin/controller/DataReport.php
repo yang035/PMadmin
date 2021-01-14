6881f973133b064b57fb2d99de1abffc171e08d6
ernest <ernest96@yeah.net>
2021-01-14 16:41:19 +0800

    数据统计调整

--- app/admin/controller/DataReport.php
+++ app/admin/controller/DataReport.php
@@ -62,6 +62,24 @@ class DataReport extends Admin
      * 注册公司数
      */
     public function getCompanyCount(){
+        $params = $this->request->post();
+        if (isset($params['p']) && 1 == $params['p']){
+            $fields = "COUNT(id) c,FROM_UNIXTIME(create_time,'%Y%m') m";
+            $data = AdminCompany::field($fields)->group('m')->select();
+            $tmp = [];
+            if ($data){
+                $xuni = [0];
+                foreach ($data as $k=>$v) {
+                    $tmp[$v['m']] = $v['c'];
+                    $xuni[$k+1] = $xuni[$k]+$v['c'];
+                }
+                array_shift($xuni);
+            }
+            return [
+                'leg'=>array_keys($tmp),
+                'ser'=>$xuni,
+            ];
+        }
         $fields = "COUNT(id) c,FROM_UNIXTIME(create_time,'%Y%m') m";
         $data = AdminCompany::field($fields)->group('m')->select();
         $tmp = [];
@@ -82,6 +100,24 @@ class DataReport extends Admin
      * 注册个人数
      */
     public function getPersonCount(){
+        $params = $this->request->post();
+        if (isset($params['p']) && 1 == $params['p']){
+            $fields = "COUNT(id) c,FROM_UNIXTIME(create_time,'%Y%m') m";
+            $data = AdminUser::field($fields)->group('m')->select();
+            $tmp = [];
+            if ($data){
+                $xuni = [0];
+                foreach ($data as $k=>$v) {
+                    $tmp[$v['m']] = $v['c'];
+                    $xuni[$k+1] = $xuni[$k]+$v['c'];
+                }
+                array_shift($xuni);
+            }
+            return [
+                'leg'=>array_keys($tmp),
+                'ser'=>$xuni,
+            ];
+        }
         $fields = "COUNT(id) c,FROM_UNIXTIME(create_time,'%Y%m') m";
         $data = AdminUser::field($fields)->group('m')->select();
         $tmp = [];
@@ -102,6 +138,24 @@ class DataReport extends Admin
      * 登录次数
      */
     public function getLoginCount(){
+        $params = $this->request->post();
+        if (isset($params['p']) && 1 == $params['p']){
+            $fields = "COUNT(user_id) c,DATE_FORMAT(login_time,'%Y%m') m";
+            $data = UserLogin::field($fields)->group('m')->select();
+            $tmp = [];
+            if ($data){
+                $xuni = [0];
+                foreach ($data as $k=>$v) {
+                    $tmp[$v['m']] = $v['c'];
+                    $xuni[$k+1] = $xuni[$k]+$v['c'];
+                }
+                array_shift($xuni);
+            }
+            return [
+                'leg'=>array_keys($tmp),
+                'ser'=>$xuni,
+            ];
+        }
         $fields = "COUNT(user_id) c,DATE_FORMAT(login_time,'%Y%m') m";
         $data = UserLogin::field($fields)->group('m')->select();
         $tmp = [];
@@ -122,15 +176,29 @@ class DataReport extends Admin
      * 菜单访问量
      */
     public function getMenuCount(){
-        $fields = "COUNT(id) c,title,url,FROM_UNIXTIME(ctime,'%Y%m') m";
-        $data = AdminLog::field($fields)->group('title')->order('c desc')->limit(10)->select();
+        $params = $this->request->post();
+        if (isset($params['p']) && 1 == $params['p']){
+            $fields = "COUNT(id) c,title,url";
+            $data = AdminLog::field($fields)->group('title')->order('c desc')->limit(10)->select();
+            $tmp = [];
+            if ($data){
+                foreach ($data as $v) {
+                    $tmp[$v['title']] = $v['c'];
+                }
+            }
+            return [
+                'leg'=>array_keys($tmp),
+                'ser'=>array_values($tmp),
+            ];
+        }
+        $fields = "COUNT(DISTINCT title) c,title,url,FROM_UNIXTIME(ctime,'%Y%m') m";
+        $data = AdminLog::field($fields)->group('m')->order('c desc')->limit(10)->select();
         $tmp = [];
         if ($data){
             foreach ($data as $v) {
                 $tmp[$v['title']] = $v['c'];
             }
         }
-
         return [
             'leg'=>array_keys($tmp),
             'ser'=>array_values($tmp),
@@ -143,6 +211,24 @@ class DataReport extends Admin
      * PV
      */
     public function getPvCount(){
+        $params = $this->request->post();
+        if (isset($params['p']) && 1 == $params['p']){
+            $fields = "COUNT(id) c,FROM_UNIXTIME(ctime,'%Y%m') m";
+            $data = AdminLog::field($fields)->group('m')->select();
+            $tmp = [];
+            if ($data){
+                $xuni = [0];
+                foreach ($data as $k=>$v) {
+                    $tmp[$v['m']] = $v['c'];
+                    $xuni[$k+1] = $xuni[$k]+$v['c'];
+                }
+                array_shift($xuni);
+            }
+            return [
+                'leg'=>array_keys($tmp),
+                'ser'=>$xuni,
+            ];
+        }
         $fields = "COUNT(id) c,FROM_UNIXTIME(ctime,'%Y%m%d') m";
         $data = AdminLog::field($fields)->group('m')->select();
         $tmp = [];
@@ -163,6 +249,24 @@ class DataReport extends Admin
      * UV
      */
     public function getUvCount(){
+        $params = $this->request->post();
+        if (isset($params['p']) && 1 == $params['p']){
+            $fields = "COUNT(DISTINCT ip) c,FROM_UNIXTIME(ctime,'%Y%m') m";
+            $data = AdminLog::field($fields)->group('m')->select();
+            $tmp = [];
+            if ($data){
+                $xuni = [0];
+                foreach ($data as $k=>$v) {
+                    $tmp[$v['m']] = $v['c'];
+                    $xuni[$k+1] = $xuni[$k]+$v['c'];
+                }
+                array_shift($xuni);
+            }
+            return [
+                'leg'=>array_keys($tmp),
+                'ser'=>$xuni,
+            ];
+        }
         $fields = "COUNT(DISTINCT ip) c,FROM_UNIXTIME(ctime,'%Y%m%d') m";
         $data = AdminLog::field($fields)->group('m')->select();
         $tmp = [];
@@ -183,6 +287,24 @@ class DataReport extends Admin
      * VV
      */
     public function getVvCount(){
+        $params = $this->request->post();
+        if (isset($params['p']) && 1 == $params['p']){
+            $fields = "COUNT(user_id) c,DATE_FORMAT(login_time,'%Y%m') m";
+            $data = UserLogin::field($fields)->group('m')->select();
+            $tmp = [];
+            if ($data){
+                $xuni = [0];
+                foreach ($data as $k=>$v) {
+                    $tmp[$v['m']] = $v['c'];
+                    $xuni[$k+1] = $xuni[$k]+$v['c'];
+                }
+                array_shift($xuni);
+            }
+            return [
+                'leg'=>array_keys($tmp),
+                'ser'=>$xuni,
+            ];
+        }
         $fields = "COUNT(user_id) c,DATE_FORMAT(login_time,'%Y%m%d') m";
         $data = UserLogin::field($fields)->group('m')->select();
         $tmp = [];
@@ -203,6 +325,24 @@ class DataReport extends Admin
      * IP
      */
     public function getIpCount(){
+        $params = $this->request->post();
+        if (isset($params['p']) && 1 == $params['p']){
+            $fields = "COUNT(DISTINCT ip) c,FROM_UNIXTIME(ctime,'%Y%m') m";
+            $data = AdminLog::field($fields)->group('m')->select();
+            $tmp = [];
+            if ($data){
+                $xuni = [0];
+                foreach ($data as $k=>$v) {
+                    $tmp[$v['m']] = $v['c'];
+                    $xuni[$k+1] = $xuni[$k]+$v['c'];
+                }
+                array_shift($xuni);
+            }
+            return [
+                'leg'=>array_keys($tmp),
+                'ser'=>$xuni,
+            ];
+        }
         $fields = "COUNT(DISTINCT ip) c,FROM_UNIXTIME(ctime,'%Y%m%d') m";
         $data = AdminLog::field($fields)->group('m')->select();
         $tmp = [];
@@ -223,7 +363,25 @@ class DataReport extends Admin
      * 商品订单量
      */
     public function getShopOrderCount(){
-        $fields = "COUNT(id) c,FROM_UNIXTIME(create_time,'%Y%m%d') m";
+        $params = $this->request->post();
+        if (isset($params['p']) && 1 == $params['p']){
+            $fields = "COUNT(id) c,FROM_UNIXTIME(create_time,'%Y%m') m";
+            $data = \app\admin\model\ShopOrder::field($fields)->group('m')->select();
+            $tmp = [];
+            if ($data){
+                $xuni = [0];
+                foreach ($data as $k=>$v) {
+                    $tmp[$v['m']] = $v['c'];
+                    $xuni[$k+1] = $xuni[$k]+$v['c'];
+                }
+                array_shift($xuni);
+            }
+            return [
+                'leg'=>array_keys($tmp),
+                'ser'=>$xuni,
+            ];
+        }
+        $fields = "COUNT(id) c,FROM_UNIXTIME(create_time,'%Y%m') m";
         $data = \app\admin\model\ShopOrder::field($fields)->group('m')->select();
         $tmp = [];
         if ($data){
@@ -243,6 +401,24 @@ class DataReport extends Admin
      * 套餐订单量
      */
     public function getMealOrderCount(){
+        $params = $this->request->post();
+        if (isset($params['p']) && 1 == $params['p']){
+            $fields = "COUNT(id) c,FROM_UNIXTIME(create_time,'%Y%m%d') m";
+            $data = \app\admin\model\MealOrder::field($fields)->group('m')->select();
+            $tmp = [];
+            if ($data){
+                $xuni = [0];
+                foreach ($data as $k=>$v) {
+                    $tmp[$v['m']] = $v['c'];
+                    $xuni[$k+1] = $xuni[$k]+$v['c'];
+                }
+                array_shift($xuni);
+            }
+            return [
+                'leg'=>array_keys($tmp),
+                'ser'=>$xuni,
+            ];
+        }
         $fields = "COUNT(id) c,FROM_UNIXTIME(create_time,'%Y%m%d') m";
         $data = \app\admin\model\MealOrder::field($fields)->group('m')->select();
         $tmp = [];
--- app/admin/view/data_report/index1.php
+++ app/admin/view/data_report/index1.php
@@ -1,31 +1,13 @@
-<div id="total_company_count" style="height: 300px;width: 33%;float: left"></div>
-<div id="company_count" style="height: 300px;width: 33%;float: left"></div>
 <div id="user_count" style="height: 300px;width: 33%;float: left"></div>
-<div id="total_person_count" style="height: 300px;width: 33%;float: left"></div>
+<div id="company_count" style="height: 300px;width: 33%;float: left"></div>
 <div id="person_count" style="height: 300px;width: 33%;float: left"></div>
-<hr>
-<div id="total_login_count" style="height: 300px;width: 33%;float: left"></div>
 <div id="login_count" style="height: 300px;width: 33%;float: left"></div>
-<hr>
-<div id="total_shoporder_count" style="height: 300px;width: 33%;float: left"></div>
 <div id="shoporder_count" style="height: 300px;width: 33%;float: left"></div>
-<hr>
-<div id="total_mealorder_count" style="height: 300px;width: 33%;float: left"></div>
 <div id="mealorder_count" style="height: 300px;width: 33%;float: left"></div>
-<hr>
-<div id="total_menu_count" style="height: 300px;width: 33%;float: left"></div>
 <div id="menu_count" style="height: 300px;width: 33%;float: left"></div>
-<hr>
-<div id="total_pv_count" style="height: 300px;width: 33%;float: left"></div>
 <div id="pv_count" style="height: 300px;width: 33%;float: left"></div>
-<hr>
-<div id="total_uv_count" style="height: 300px;width: 33%;float: left"></div>
 <div id="uv_count" style="height: 300px;width: 33%;float: left"></div>
-<hr>
-<div id="total_vv_count" style="height: 300px;width: 33%;float: left"></div>
 <div id="vv_count" style="height: 300px;width: 33%;float: left"></div>
-<hr>
-<div id="total_ip_count" style="height: 300px;width: 33%;float: left"></div>
 <div id="ip_count" style="height: 300px;width: 33%;float: left"></div>
 
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/echarts@4/dist/echarts.min.js"></script>
@@ -44,31 +26,21 @@
         var $ = layui.jquery, laydate = layui.laydate;
     });
     // var _url = "{:url('admin/Index/getApprovalCount')}";
-    pie_chart('user_count','用户对比',"{:url('admin/DataReport/getUserCount')}",'pie');
-    bar_waterfall('total_company_count','累计公司数',"{:url('admin/DataReport/getCompanyCount')}",'line',1);
-    waterfall('company_count','每月新增公司',"{:url('admin/DataReport/getCompanyCount')}",'bar');
-    bar_waterfall('total_person_count','累计人员数',"{:url('admin/DataReport/getPersonCount')}",'line',1);
-    waterfall('person_count','每月新增人员',"{:url('admin/DataReport/getPersonCount')}",'bar');
-    bar_waterfall('total_login_count','累计登录数',"{:url('admin/DataReport/getLoginCount')}",'line',1);
-    waterfall('login_count','每月登录次数',"{:url('admin/DataReport/getLoginCount')}",'bar');
-    bar_waterfall('total_shoporder_count','累计商品订单量',"{:url('admin/DataReport/getShopOrderCount')}",'line',1);
-    waterfall('shoporder_count','每月商品订单',"{:url('admin/DataReport/getShopOrderCount')}",'bar');
-    bar_waterfall('total_mealorder_count','累计套餐订单量',"{:url('admin/DataReport/getMealOrderCount')}",'line',1);
-    waterfall('mealorder_count','每天套餐订单',"{:url('admin/DataReport/getMealOrderCount')}",'bar');
-    bar_waterfall('total_menu_count','累计菜单访问前10',"{:url('admin/DataReport/getMenuCount')}",'line',1);
-    waterfall('menu_count','每月菜单访问前10',"{:url('admin/DataReport/getMenuCount')}",'bar');
-    bar_waterfall('total_pv_count','累计PV',"{:url('admin/DataReport/getPvCount')}",'line',1);
-    waterfall('pv_count','PV',"{:url('admin/DataReport/getPvCount')}",'line');
-    bar_waterfall('total_uv_count','累计UV',"{:url('admin/DataReport/getUvCount')}",'line',1);
-    waterfall('uv_count','UV',"{:url('admin/DataReport/getUvCount')}",'line');
-    bar_waterfall('total_vv_count','累计VV',"{:url('admin/DataReport/getVvCount')}",'line',1);
-    waterfall('vv_count','VV',"{:url('admin/DataReport/getVvCount')}",'line');
-    bar_waterfall('total_ip_count','累计IP',"{:url('admin/DataReport/getIpCount')}",'line',1);
-    waterfall('ip_count','IP',"{:url('admin/DataReport/getIpCount')}",'line');
+    pie_chart('user_count','用户对比',"{:url('admin/DataReport/getUserCount')}");
+    waterfall('company_count','每月新增公司',"{:url('admin/DataReport/getCompanyCount')}");
+    waterfall('person_count','每月新增人员',"{:url('admin/DataReport/getPersonCount')}");
+    waterfall('login_count','每月登录次数',"{:url('admin/DataReport/getLoginCount')}");
+    waterfall('shoporder_count','每天商品订单量',"{:url('admin/DataReport/getShopOrderCount')}");
+    waterfall('mealorder_count','每天套餐订单量',"{:url('admin/DataReport/getMealOrderCount')}");
+    waterfall('menu_count','每月菜单访问前10',"{:url('admin/DataReport/getMenuCount')}");
+    waterfall('pv_count','PV',"{:url('admin/DataReport/getPvCount')}");
+    waterfall('uv_count','UV',"{:url('admin/DataReport/getUvCount')}");
+    waterfall('vv_count','VV',"{:url('admin/DataReport/getVvCount')}");
+    waterfall('ip_count','IP',"{:url('admin/DataReport/getIpCount')}");
 
 
 
-    function pie_chart(id,title,_url,type) {
+    function pie_chart(id,title,_url) {
         var dom = document.getElementById(id);
         var myChart = echarts.init(dom);
         var app = {};
@@ -91,7 +63,7 @@
             },
             series : [
                 {
-                    type: type,
+                    type: 'pie',
                     radius : '65%',
                     center: ['50%', '50%'],
                     selectedMode: 'single',
@@ -238,70 +210,70 @@
         });
     }
 
-    function waterfall(id,title,_url,type) {
+    function waterfall(id,title,_url) {
         var dom = document.getElementById(id);
         var myChart = echarts.init(dom);
         var app = {};
         option = null;
         myChart.setOption({
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'shadow',
-                    label: {
-                        show: true
-                    }
-                }
-            },
-            toolbox: {
-                show: true,
-                feature: {
-                    magicType: {show: true, type: ['line', 'bar']},
-                    saveAsImage: {show: true}
-                }
-            },
-            calculable: true,
-            legend: {
-                data: [title],
-                itemGap: 5
-            },
-            grid: {
-                top: '12%',
-                left: '1%',
-                right: '10%',
-                containLabel: true
-            },
-            xAxis: [
-                {
-                    type: 'category',
-                    data: []
-                }
-            ],
-            yAxis: [
-                {
-                    type: 'value',
-                    name: title,
-                    axisLabel: {
-                        formatter: function (a) {
-                            a = +a;
-                            return isFinite(a)
-                                ? echarts.format.addCommas(+a / 1)
-                                : '';
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'shadow',
+                        label: {
+                            show: true
                         }
                     }
-                }
-            ],
-            series: [
-                {
-                    name: title,
-                    type: type,
-                    label: {
-                        show: true,
-                        position: 'inside'
-                    },
-                    data: []
-                }
-            ]
+                },
+                toolbox: {
+                    show: true,
+                    feature: {
+                        magicType: {show: true, type: ['line', 'bar']},
+                        saveAsImage: {show: true}
+                    }
+                },
+                calculable: true,
+                legend: {
+                    data: [title],
+                    itemGap: 5
+                },
+                grid: {
+                    top: '12%',
+                    left: '1%',
+                    right: '10%',
+                    containLabel: true
+                },
+                xAxis: [
+                    {
+                        type: 'category',
+                        data: []
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value',
+                        name: title,
+                        axisLabel: {
+                            formatter: function (a) {
+                                a = +a;
+                                return isFinite(a)
+                                    ? echarts.format.addCommas(+a / 1)
+                                    : '';
+                            }
+                        }
+                    }
+                ],
+                series: [
+                    {
+                        name: title,
+                        type: 'line',
+                        label: {
+                            show: true,
+                            position: 'inside'
+                        },
+                        data: []
+                    }
+                ]
         });
         myChart.showLoading();    //数据加载完之前先显示一段简单的loading动画
 
@@ -310,102 +282,6 @@
             async: true,            //异步请求（同步请求将会锁住浏览器，用户其他操作必须等待请求完成才可以执行）
             url: _url,    //请求发送到TestServlet处
             data: {},
-            dataType: "json",        //返回数据形式为json
-            success: function (result) {
-                //请求成功时执行该函数内容，result即为服务器返回的json对象
-                if (result) {
-                    myChart.hideLoading();    //隐藏加载动画
-                    myChart.setOption({        //加载数据图表
-                        xAxis: {
-                            data: result.leg
-                        },
-                        series: [{
-                            data: result.ser
-                        }]
-                    });
-
-                }
-            },
-            error: function (errorMsg) {
-                //请求失败时执行该函数
-                alert("图表请求数据失败!");
-                myChart.hideLoading();
-            }
-        });
-    }
-
-    function bar_waterfall(id,title,_url,type,p=0) {
-        var dom = document.getElementById(id);
-        var myChart = echarts.init(dom);
-        var app = {};
-        option = null;
-        myChart.setOption({
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'shadow',
-                    label: {
-                        show: true
-                    }
-                }
-            },
-            toolbox: {
-                show: true,
-                feature: {
-                    magicType: {show: true, type: ['line', 'bar']},
-                    saveAsImage: {show: true}
-                }
-            },
-            calculable: true,
-            legend: {
-                data: [title],
-                itemGap: 5
-            },
-            grid: {
-                top: '12%',
-                left: '1%',
-                right: '10%',
-                containLabel: true
-            },
-            xAxis: [
-                {
-                    type: 'category',
-                    data: []
-                }
-            ],
-            yAxis: [
-                {
-                    type: 'value',
-                    name: title,
-                    axisLabel: {
-                        formatter: function (a) {
-                            a = +a;
-                            return isFinite(a)
-                                ? echarts.format.addCommas(+a / 1)
-                                : '';
-                        }
-                    }
-                }
-            ],
-            series: [
-                {
-                    name: title,
-                    type: type,
-                    label: {
-                        show: true,
-                        position: 'inside'
-                    },
-                    data: []
-                }
-            ]
-        });
-        myChart.showLoading();    //数据加载完之前先显示一段简单的loading动画
-
-        $.ajax({
-            type: "post",
-            async: true,            //异步请求（同步请求将会锁住浏览器，用户其他操作必须等待请求完成才可以执行）
-            url: _url,    //请求发送到TestServlet处
-            data: {p:p},
             dataType: "json",        //返回数据形式为json
             success: function (result) {
                 //请求成功时执行该函数内容，result即为服务器返回的json对象
