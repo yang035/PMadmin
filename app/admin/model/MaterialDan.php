beb125b98eafb150793c1514ca1ff128900322d8
ernest <ernest96@yeah.net>
2021-01-19 19:00:59 +0800

    供货单报表

--- app/admin/model/MaterialDan.php
+++ app/admin/model/MaterialDan.php
@@ -1,16 +1,55 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/10/8
- * Time: 15:00
- */
 
 namespace app\admin\model;
 
-
 use think\Model;
+use think\Loader;
+use think\Db;
 
 class MaterialDan extends Model
 {
+    public static function getProject($type = 0){
+        $w1 = [
+            'cid'=>session('admin_user.cid'),
+        ];
+        $project_id = self::where($w1)->column('distinct(project_id) project_id');
+        $str = "<option value=''>选择</option>";
+        if ($project_id){
+            $w2 = [
+                'id'=>['in',$project_id],
+            ];
+            $p_data = Project::where($w2)->column('name','id');
+            if ($type){
+                return $p_data;
+            }
+            foreach ($p_data as $k=>$v){
+                $str .= "<option value='".$k."'>".$v."</option>";
+            }
+        }
+        return $str;
+    }
+    public static function getTotal(){
+        $where = [
+            'cid'=>session('admin_user.cid'),
+        ];
+        $data = self::where($where)->group('project_id')->column('SUM(caigou_zongjia)','project_id');
+        return $data;
+    }
+
+    public static function getCaigouDetail($project_id){
+        $where = [
+            'cid'=>session('admin_user.cid'),
+            'project_id'=>$project_id,
+        ];
+        $fields = "project_id,name,SUM(caigou_shuliang) as caigou_shuliang,SUM(caigou_zongjia) as caigou_zongjia";
+        $data = self::where($where)->field($fields)->group('name')->select();
+        $res = [];
+        if ($data){
+            foreach ($data as $k=>$v){
+                $res[$v['name']] = $v;
+            }
+        }
+        return $res;
+    }
+
 }
---
+++ app/admin/view/material_dan/warning.php
@@ -0,0 +1,63 @@
+<style>
+    .layui-form-item .layui-input-inline {
+        float: left;
+        width: auto;
+    }
+</style>
+{include file="block/layui" /}
+<style type="text/css">
+    a:hover{
+        cursor: pointer;
+    }
+</style>
+<div class="page-toolbar">
+    <div class="page-filter">
+        <form class="layui-form layui-form-pane" action="{:url()}" method="get" id="hisi-table-search">
+            <div class="layui-form-item">
+                <label class="layui-form-label">项目名</label>
+                <div class="layui-input-inline">
+                    <select name="project_id" class="layui-input field-project_id" type="select" lay-filter="project" lay-search>
+                        {$project_select}
+                    </select>
+                </div>
+                <input type="hidden" class="field-cat_id" name="cat_id">
+                <button type="submit" class="layui-btn layui-btn-normal">搜索</button>
+            </div>
+        </form>
+    </div>
+    <div class="layui-btn-group fl">
+        <!--        <a href="{:url('add')}" class="layui-btn layui-btn-primary layui-icon layui-icon-add-circle-fine">&nbsp;添加</a>-->
+        <!--        <a data-href="{:url('status?table=shopping_record&val=1')}" class="layui-btn layui-btn-primary j-page-btns layui-icon layui-icon-play" data-table="dataTable">&nbsp;启用</a>-->
+        <!--        <a data-href="{:url('status?table=shopping_record&val=0')}" class="layui-btn layui-btn-primary j-page-btns layui-icon layui-icon-pause" data-table="dataTable">&nbsp;禁用</a>-->
+        <!--        <a data-href="{:url('del')}" class="layui-btn layui-btn-primary j-page-btns confirm layui-icon layui-icon-close red">&nbsp;删除</a>-->
+    </div>
+</div>
+<table id="dataTable" class="layui-table" lay-filter="table1"></table>
+<script type="text/html" id="statusTpl">
+    <input type="checkbox" name="status" value="{{ d.status }}" lay-skin="switch" lay-filter="switchStatus" lay-text="正常|关闭" {{ d.status == 1 ? 'checked' : '' }} data-href="{:url('status')}?table=shopping_record&id={{ d.id }}">
+</script>
+<script type="text/html" title="操作按钮模板" id="buttonTpl">
+    <a href="{:url('analyse')}?project_id={{ d.project_id }}" class="layui-btn layui-btn-xs layui-btn-normal">明细</a>
+    <!--    <a href="{:url('del')}?id={{ d.id }}" class="layui-btn layui-btn-xs layui-btn-danger j-tr-del">删除</a>-->
+</script>
+<script type="text/javascript">
+    var _url = "{:url()}?name={$Request.param.name}";
+    layui.use(['table'], function() {
+        var table = layui.table;
+        table.render({
+            elem: '#dataTable'
+            ,url: _url //数据接口
+            ,page: true //开启分页
+            ,limit: 30
+            ,text: {
+                none : '暂无相关数据'
+            }
+            ,cols: [[ //表头
+                {type:'checkbox'}
+                ,{field: 'project_name', title: '项目'}
+                ,{field: 'caigou_zongjia',  title: '总价(元)'}
+                ,{title: '操作', templet: '#buttonTpl'}
+            ]]
+        });
+    });
+</script>
