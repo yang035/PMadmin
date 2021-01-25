e5c0d1e52f9f4cd38cd1f78279632400ed793efd
ernest <ernest96@yeah.net>
2021-01-25 09:42:23 +0800

    测试调整

--- app/admin/controller/T.php
+++ app/admin/controller/T.php
@@ -12,6 +12,7 @@ namespace app\admin\controller;
 use think\Controller;
 use app\admin\model\Project;
 use app\admin\model\SubjectItem;
+use think\Db;
 
 class T extends Controller
 {
@@ -48,6 +49,85 @@ class T extends Controller
         $mins = (int)(($time2 - $time1 - $days * 3600 * 24 - $hours * 3600) / 60);
         $sec = $time2 - $time1 - $days * 3600 * 24 - $hours * 3600 - $mins * 60;
         return $days . '天' . $hours . '小时' . $mins . '分钟' . $sec . '秒';
+    }
+
+    public function editApproval(){
+        $approval = \db('approval')->column('class_type','id');
+        foreach ($approval as $k=>$v) {
+            switch ($v){
+                case 1:
+                    $table = 'approval_leave';
+                    break;
+                case 2:
+                    $table = 'approval_expense';
+                    break;
+                case 3:
+                    $table = 'approval_cost';
+                    break;
+                case 4:
+                    $table = 'approval_business';
+                    break;
+                case 5:
+                    $table = 'approval_procurement';
+                    break;
+                case 6:
+                    $table = 'approval_overtime';
+                    break;
+                case 7:
+                    $table = 'approval_goout';
+                    break;
+                case 8:
+                    $table = 'approval_usecar';
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    $table = 'approval_goods';
+                    break;
+                case 12:
+                    $table = 'approval_print';
+                    break;
+                case 13:
+                    $table = 'approval_dispatch';
+                    break;
+                case 14:
+                    $table = 'approval_borrow';
+                    break;
+                case 15:
+                    $table = 'approval_backleave';
+                    break;
+                case 16:
+                    $table = 'approval_bills';
+                    break;
+                case 17:
+                    $table = 'approval_tixian';
+                    break;
+                case 18:
+                    $table = 'approval_leaveoffice';
+                    break;
+                case 19:
+                    $table = 'approval_invoice';
+                    break;
+                case 20:
+                    $table = 'approval_waybill';
+                    break;
+                case 21:
+                    $table = 'approval_applypay';
+                    break;
+                case 22:
+                    $table = 'approval_waybill';
+                    break;
+                case 23:
+                    $table = 'approval_applypay';
+                    break;
+            }
+            $reason = \db($table)->where(['aid'=>$k])->column('reason');
+            if ($reason){
+                \db('approval')->where(['id'=>$k])->setField('reason',$reason[0]);
+            }
+        }
     }
 
 
